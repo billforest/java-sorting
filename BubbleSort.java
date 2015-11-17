b7be97f87fc5ea2982f4d75ba5d2de855067a98a
jaehyung Lim <jaehyunglimsp15@gpa.ac.kr>
2015-11-17 13:57:21 +0900

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+// BubbleSort.java
+
+public class BubbleSort extends Sorts {
+  
+  // instance variables
+  private int[] bubble = new int[12];
+  private String name;
+  
+  // constructor
+  public BubbleSort() {
+    bubble = super.array;
+    name = "Bubble";
+  }
+  
+  // methods
+  public void sort() {
+    // Timing: Best = 0(n); AVE/WORST = 0(n^2)
+    
+    System.out.println( "Doing " + name + " Sort:" );
+    boolean continueLoop;
+    do {
+      
+      continueLoop = false;
+      for( int i = 0; i < bubble.length-1; i++ ) {
+        if( bubble[i] > bubble[i+1] ) {
+          super.swap( i, i+1 );
+          continueLoop = true;
+        }
+      }
+    } while( continueLoop );
+    
+  } // end sort()
+  
+} // END BubbleSort
---
+++ RunSorts.java
@@ -0,0 +1,30 @@
+/* RunSorts.java
+ * Description: Running various sorting methods on the same array 
+ * Name: Jaehyung Lim
+ * Date: Nov 17, 2015
+ */
+
+public class RunSorts {
+  
+  public static void main( String[] args ) {
+    
+    // Bublle sort
+    BubbleSort b = new BubbleSort();
+    System.out.println( b );
+    b.sort();
+    System.out.println( b );
+    
+    // Selection Sort
+    
+    // Insertion Sort
+    
+    // Quick Sort
+    
+    // Merge Sort
+    
+    // Rdix Sort???
+    
+    
+  } // end main method
+  
+} // end RunSorts
